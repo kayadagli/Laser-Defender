a061732a29bc308ffd130a2ae89efe4ab2c49c3e
Kaya Dağlı <kayadagli27@gmail.com>
2021-04-05 16:28:12 +0300

    Laser Defender

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+
+    [SerializeField] float backgroundScroolSpeed = 0.5f;
+    Material myMaterial;
+    Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(0f, backgroundScroolSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
